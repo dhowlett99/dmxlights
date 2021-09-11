760e56f54da85d7432086a161448110ad198528f
Derek Howlett <derek@howlett.com>
2021-09-11 23:26:44 +0100

    fix bounce for scanners

--- pkg/sequence/sequence.go
+++ pkg/sequence/sequence.go
@@ -173,7 +173,6 @@ func calculatePositions(steps []common.Step, bounce bool) (map[int][]common.Posi
 					position.Color.B = color.B
 					positionsOut[counter] = append(positionsOut[counter], position)
 					if fixture.Type != "scanner" {
-						//positionsOut[counter] = append(positionsOut[counter], position)
 						counter = counter + 14
 					}
 				}
@@ -201,9 +200,14 @@ func calculatePositions(steps []common.Step, bounce bool) (map[int][]common.Posi
 						position.Color.G = color.G
 						position.Color.B = color.B
 						positionsOut[counter] = append(positionsOut[counter], position)
-						counter = counter + 14
+						if fixture.Type != "scanner" {
+							counter = counter + 14
+						}
 					}
 				}
+			}
+			if step.Type == "scanner" {
+				counter = counter + 14
 			}
 		}
 	}
