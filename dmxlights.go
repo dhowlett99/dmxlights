d6b81f7d80326a1c0e76f98c355134f5ecd6c6f6
Derek Howlett <derek@howlett.com>
2021-09-25 14:12:45 +0100

    restore blackout after reload

--- dmxlights.go
+++ dmxlights.go
@@ -285,6 +285,12 @@ func main() {
 						// Load the config.
 						config.AskToLoadConfig(commandChannels, hit.X, hit.Y)
 
+						// Restore the blackout setting.
+						cmd = common.Command{
+							Blackout: blackout,
+						}
+						common.SendCommandToAllSequence(selectedSequence, cmd, commandChannels)
+
 						// Reset flash buttons
 						for x := 0; x < 9; x++ {
 							for y := 0; y < 9; y++ {
