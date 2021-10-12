ec847be27628c47895e9eb3d4e295f906d27551a
Derek Howlett <derek@howlett.com>
2021-10-12 11:22:24 +0100

    add portaudio debug

--- pkg/sound/sound.go
+++ pkg/sound/sound.go
@@ -12,21 +12,25 @@ func NewSoundTrigger(soundTriggers []*common.Trigger, channels common.Channels) 
 
 	go func() {
 
-		fmt.Println("Starting Sound System")
+		fmt.Printf("Starting Sound System Version %s\n", portaudio.VersionText())
 
-		portaudio.Initialize()
+		err := portaudio.Initialize()
+		if err != nil {
+			fmt.Printf("error: portaudio: failed to initialise portaudio\n")
+		}
+
 		defer portaudio.Terminate()
 		// Making the buffer bigger makes the music trigger have less latency.
 		in := make([]int32, 128)
 		stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(in), in)
 		if err != nil {
-			fmt.Printf("error: failed to initialise portaudio\n")
+			fmt.Printf("error: portaudio: failed to open default stream \n")
 		}
 
 		// Start listening on the microphone input.
 		stream.Start()
 		if err != nil {
-			fmt.Printf("error: failed to start stream\n")
+			fmt.Printf("error: portaudio: failed to start stream\n")
 		}
 
 		defer stream.Close()
@@ -34,7 +38,7 @@ func NewSoundTrigger(soundTriggers []*common.Trigger, channels common.Channels) 
 		for {
 			stream.Read()
 			if err != nil {
-				fmt.Printf("error: failed to read audio stream\n")
+				fmt.Printf("error: portaudio: failed to read audio stream\n")
 			}
 
 			if in[0] > 1000000000 {
