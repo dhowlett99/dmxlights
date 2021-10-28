9acb65ddbc9876e2e56fcadad9f057730e63046e
Derek Howlett <derek@howlett.com>
2021-10-28 18:27:04 +0100

    remove old values

--- pkg/sound/sound.go
+++ pkg/sound/sound.go
@@ -17,8 +17,6 @@ func NewSoundTrigger(soundTriggers []*common.Trigger, channels common.Channels) 
 
 	go func() {
 
-		//gain := []float32{0.01, 0.02, 0.03, 0.05, 0.07, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.25, 0.30}
-
 		gain := []float32{0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15}
 
 		fmt.Printf("Starting Sound System Version %s\n", portaudio.VersionText())
