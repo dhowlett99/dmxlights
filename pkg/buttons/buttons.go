5a9f40f81537a0cda91cf6384d6133c510d9a0fe
dhowlett <derek@howlett.com>
2023-09-11 16:00:38 -0400

    WIP

--- pkg/buttons/buttons.go
+++ pkg/buttons/buttons.go
@@ -1918,29 +1918,122 @@ func ShowRGBColorSelectionButtons(master int, targetSequence common.Sequence, di
 	if debug {
 		fmt.Printf("Show Color Selection Buttons\n")
 	}
-	// Check if we need to flash this button.
-	for myFixtureNumber, lamp := range targetSequence.RGBAvailableColors {
 
-		for index, availableColor := range targetSequence.RGBAvailableColors {
-			for _, sequenceColor := range targetSequence.CurrentColors {
-				if availableColor.Color == sequenceColor {
-					if debug {
-						fmt.Printf("myFixtureNumber %d   current color %+v\n", myFixtureNumber, sequenceColor)
-					}
-					if myFixtureNumber == index {
-						lamp.Flash = true
-					}
-				}
+	lights := []int{
+		0, 0, 0, 0, 0, 1, 1, 2,
+		2, 3, 4, 5, 6, 7, 8, 9,
+		11, 12, 13, 15, 17, 18, 20, 22,
+		24, 26, 28, 30, 32, 35, 37, 39,
+		42, 44, 47, 49, 52, 55, 58, 60,
+		63, 66, 69, 72, 75, 78, 81, 85,
+		88, 91, 94, 97, 101, 104, 107, 111,
+		114, 117, 121, 124, 127, 131, 134, 137,
+		141, 144, 147, 150, 154, 157, 160, 163,
+		167, 170, 173, 176, 179, 182, 185, 188,
+		191, 194, 197, 200, 202, 205, 208, 210,
+		213, 215, 217, 220, 222, 224, 226, 229,
+		231, 232, 234, 236, 238, 239, 241, 242,
+		244, 245, 246, 248, 249, 250, 251, 251,
+		252, 253, 253, 254, 254, 255, 255, 255,
+		255, 255, 255, 255, 254, 254, 253, 253,
+		252, 251, 251, 250, 249, 248, 246, 245,
+		244, 242, 241, 239, 238, 236, 234, 232,
+		231, 229, 226, 224, 222, 220, 217, 215,
+		213, 210, 208, 205, 202, 200, 197, 194,
+		191, 188, 185, 182, 179, 176, 173, 170,
+		167, 163, 160, 157, 154, 150, 147, 144,
+		141, 137, 134, 131, 127, 124, 121, 117,
+		114, 111, 107, 104, 101, 97, 94, 91,
+		88, 85, 81, 78, 75, 72, 69, 66,
+		63, 60, 58, 55, 52, 49, 47, 44,
+		42, 39, 37, 35, 32, 30, 28, 26,
+		24, 22, 20, 18, 17, 15, 13, 12,
+		11, 9, 8, 7, 6, 5, 4, 3,
+		2, 2, 1, 1, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+	}
+
+	// lightsUp := common.GetFadeValues(180, 255, 8, false)
+	// lightsDown := common.GetFadeValues(180, 255, 8, true)
+	// lights := lightsUp
+	// lights = append(lightsUp, lightsDown...)
+
+	angle := 0
+	for x := 0; x < 8; x++ {
+		for y := 0; y < 8; y++ {
+			r, g, b := sineLED(angle, lights)
+			if angle > 360 {
+				break
 			}
-		}
-		if lamp.Flash {
-			Black := common.Color{R: 0, G: 0, B: 0}
-			common.FlashLight(myFixtureNumber, displaySequence, lamp.Color, Black, eventsForLaunchpad, guiButtons)
-		} else {
-			common.LightLamp(common.ALight{X: myFixtureNumber, Y: displaySequence, Brightness: master, Red: lamp.Color.R, Green: lamp.Color.G, Blue: lamp.Color.B}, eventsForLaunchpad, guiButtons)
+			angle += 3
+			fmt.Printf("Red %d Green %d Blue %d\n", r, g, b)
+			common.LightLamp(common.ALight{X: x, Y: y, Brightness: master, Red: r, Green: g, Blue: b}, eventsForLaunchpad, guiButtons)
+			common.LabelButton(x, y, "", guiButtons)
 		}
 	}
+
+	// // Check if we need to flash this button.
+	// for myFixtureNumber, lamp := range targetSequence.RGBAvailableColors {
+
+	// 	for index, availableColor := range targetSequence.RGBAvailableColors {
+	// 		for _, sequenceColor := range targetSequence.CurrentColors {
+	// 			if availableColor.Color == sequenceColor {
+	// 				if debug {
+	// 					fmt.Printf("myFixtureNumber %d   current color %+v\n", myFixtureNumber, sequenceColor)
+	// 				}
+	// 				if myFixtureNumber == index {
+	// 					lamp.Flash = true
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	if lamp.Flash {
+	// 		Black := common.Color{R: 0, G: 0, B: 0}
+	// 		common.FlashLight(myFixtureNumber, displaySequence, lamp.Color, Black, eventsForLaunchpad, guiButtons)
+	// 	} else {
+	// 		common.LightLamp(common.ALight{X: myFixtureNumber, Y: displaySequence, Brightness: master, Red: lamp.Color.R, Green: lamp.Color.G, Blue: lamp.Color.B}, eventsForLaunchpad, guiButtons)
+	// 	}
+	// }
 }
+
+func sineLED(angle int, lights []int) (r, g, b int) {
+
+	return lights[(angle+120)%360], lights[angle], lights[(angle+240)%360]
+}
+
+// func sineLED(angle int) (r, g, b int) {
+
+// 	red := float64((angle + 120) % 360)
+// 	fmt.Printf("Red %f\n", red)
+
+// 	fmt.Printf("Sin Red %d\n", r)
+
+// 	green := float64(angle)
+// 	// Convert angle to radians
+// 	thetaGreen := (green - 90) * math.Pi / 180
+// 	g = int(255 - math.Sin(float64(thetaGreen)))
+
+// 	blue := float64((angle + 240) % 360)
+// 	// Convert angle to radians
+// 	thetaBlue := (blue - 90) * math.Pi / 180
+// 	b = int(255 - math.Sin(thetaBlue))
+
+// 	return r, g, b
+// }
 
 // For the given sequence show the available scanner selection colors on the relevant buttons.
 func ShowSelectFixtureButtons(targetSequence common.Sequence, displaySequence int, this *CurrentState, eventsForLaunchpad chan common.ALight, action string, guiButtons chan common.ALight) int {
