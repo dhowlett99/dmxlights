b53eb369c6d0e96f2bb3122f2bc4a26caf066e22
Derek Howlett <derek@howlett.com>
2025-06-22 12:46:04 +0100

    make sure we don't recall a preset using pause button on launchpad.

--- pkg/buttons/buttons.go
+++ pkg/buttons/buttons.go
@@ -217,6 +217,7 @@ func ProcessButtons(X int, Y int,
 	// recall (short press) or delete (long press) the preset. & Not the pause button.
 	if X >= 100 && X < 108 &&
 		(Y > 3 && Y < 7) &&
+		!(X == 107 && Y == 6) &&
 		!(X == 7 && Y == 6) {
 
 		// Remove the button off offset.
